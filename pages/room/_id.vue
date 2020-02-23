cc0a683ed3ad0e7500f48ee30e7e1a046ad4d6d4
edy4c7 <edy4c7@gmail.com>
2020-02-23 13:04:42 +0900

    チャットできるようになった

--- pages/room/_id.vue
+++ pages/room/_id.vue
@@ -1,19 +1,62 @@
 <template>
-  <form action="">
-    <input id="message" type="text" name="message">
-    <button type="submit">
-      send
-    </button>
-  </form>
+  <div>
+    <form @submit.prevent="send" action="">
+      <input id="message" v-model="message" type="text" name="message">
+      <button :disabled="!isJoined" type="submit">
+        send
+      </button>
+      <ul>
+        <li v-for="(m, i) in messages" :key="i">
+          {{ m }}
+        </li>
+      </ul>
+    </form>
+    <input v-model="url" type="text" readonly>
+    <form @submit.prevent="leave" action="">
+      <button>leave</button>
+    </form>
+  </div>
 </template>
 
 <script>
 import io from 'socket.io-client'
 
 export default {
+  data () {
+    return {
+      isJoined: false,
+      message: '',
+      messages: [],
+      socket: io({
+        query: {
+          roomId: this.$route.params.id
+        }
+      })
+    }
+  },
+  computed: {
+    url () {
+      return `${process.env.baseUrl}/join?roomId=${this.$route.params.id}`
+    }
+  },
   mounted () {
-    /* eslint no-unused-vars: 1 */
-    const socket = io()
+    /* eslint no-unused-vars: 0 */
+    this.socket.on('joined', () => {
+      this.isJoined = true
+    })
+    this.socket.on('incoming', (param) => {
+      this.messages.push(param.message)
+    })
+  },
+  methods: {
+    send () {
+      this.socket.emit('send', {
+        message: this.message
+      })
+      this.message = ''
+    },
+    leave () {
+    }
   }
 }
 
--- server/socket.js
+++ server/socket.js
@@ -6,6 +6,21 @@ io.use((socket, next) => {
 })
 
 io.on('connection', (socket) => {
+  const roomId = socket.handshake.query.roomId
+
+  socket.join(roomId, () => {
+    io.to(socket.id).emit('joined')
+  })
+  socket.on('send', (param) => {
+    io.to(roomId).emit('incoming', {
+      message: param.message
+    })
+  })
+  socket.on('leave', () => {
+    socket.leave(roomId)
+    const joinedRooms = socket.request.session.rooms || []
+    socket.request.session.rooms = joinedRooms.filter(x => x !== roomId)
+  })
 })
 
 module.exports = io
